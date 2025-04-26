42ec51facfef00d110d2e9bd8115399f80a45cfd
chen <1007449846@qq.com>
2025-04-26 16:26:23 +0800

    Update snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,10 +5,32 @@ return {
       picker = {
         sources = {
           explorer = {
+            include = { ".env" },
             layout = {
               layout = {
                 width = 0.24,
               },
+            },
+          },
+        },
+      },
+      terminal = {
+        win = {
+          position = "float",
+          width = 0.6,
+          keys = {
+            {
+              "<C-\\>",
+              "hide",
+              mode = { "t", "n" },
+            },
+            term_normal = {
+              "<esc>",
+              function()
+                vim.cmd("stopinsert")
+              end,
+              mode = "t",
+              desc = "esc to normal mode",
             },
           },
         },
