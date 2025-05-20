02257a3723b0e0444f6228ae9d026c1a36144fd2
chen <kwdiwt@sina.com>
2025-05-20 17:17:10 +0800

    feat: add config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,10 +5,33 @@ return {
       picker = {
         sources = {
           explorer = {
+            include = { ".env", "compose.override.yml" },
             layout = {
               layout = {
                 width = 0.24,
+                resize = false,
               },
+            },
+          },
+        },
+      },
+      terminal = {
+        win = {
+          position = "float",
+          width = 0.6,
+          keys = {
+            {
+              "<C-\\>",
+              "hide",
+              mode = { "t", "n" },
+            },
+            term_normal = {
+              "<esc>",
+              function()
+                vim.cmd("stopinsert")
+              end,
+              mode = "t",
+              desc = "esc to normal mode",
             },
           },
         },
