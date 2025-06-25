1ca5edae75eaf850455f21df213dc763d60d96fd
chen <kwdiwt@sina.com>
2025-06-25 23:23:55 +0800

    feat: update

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,41 +1,41 @@
 return {
-	{
-		"folke/snacks.nvim",
-		opts = {
-			picker = {
-				sources = {
-					explorer = {
-						include = { ".env", "compose.override.yml" },
-						layout = {
-							layout = {
-								width = 0.24,
-								resize = false,
-							},
-						},
-					},
-				},
-			},
-			terminal = {
-				win = {
-					position = "float",
-					width = 0.6,
-					keys = {
-						{
-							"<C-\\>",
-							"hide",
-							mode = { "t", "n" },
-						},
-						term_normal = {
-							"<esc>",
-							function()
-								vim.cmd("stopinsert")
-							end,
-							mode = "t",
-							desc = "esc to normal mode",
-						},
-					},
-				},
-			},
-		},
-	},
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            include = { "t.py", ".env", ".env.sample", ".env.example", "compose.override.yml" },
+            layout = {
+              layout = {
+                width = 0.24,
+                resize = false,
+              },
+            },
+          },
+        },
+      },
+      terminal = {
+        win = {
+          position = "float",
+          width = 0.6,
+          keys = {
+            {
+              "<C-\\>",
+              "hide",
+              mode = { "t", "n" },
+            },
+            term_normal = {
+              "<esc>",
+              function()
+                vim.cmd("stopinsert")
+              end,
+              mode = "t",
+              desc = "esc to normal mode",
+            },
+          },
+        },
+      },
+    },
+  },
 }
